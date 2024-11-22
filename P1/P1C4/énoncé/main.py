4765fb14f660e90ce9a57c5d73b0a90430559833
teddyolourou8 <151736985+teddyolourou8@users.noreply.github.com>
2024-11-23 00:31:03 +0100

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,9 @@
-# Écrivez votre code ici !
+nom = "OLOUROU"
+age = "22"
+
+print(f"Bonjour, je m'appelle {nom} et j'ai {age} ans.")
+
+age = "32"
+print(age)
+
+print(f"Bonjour, je m'appelle {nom} et j'ai {age} ans.")
